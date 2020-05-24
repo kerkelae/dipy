db431b69ccc931def4c63cc6fc23ee050c8b9ef7
kerkelae <leevi.kerkela@protonmail.com>
2020-05-24 19:23:18 +0100

    NF - created qti files

---
+++ dipy/reconst/qti.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Code for q-space trajectory imaging.
+
+For details of the model, please see "Q-space trajectory imaging for
+multidimensional diffusion MRI of the human brain" by Westin et al. (2016)
+https://doi.org/10.1016/j.neuroimage.2016.02.039
+"""
+
